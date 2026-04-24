73ab60d7bfad3bf1d86a94e0e9129b8cb90b2779
Zachary Amith <76545253+RogueZach@users.noreply.github.com>
2026-04-24 23:11:13 +0000

    reset Pantry.java and Ingredient.java to accomodate the decision to get rid of Swing for GUI, and focus on terminal. Skeleton only

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,23 +1,3 @@
 public class Ingredient {
-    private String name;
-    private double quantity;
-    private String unit;
-
-    public Ingredient(String name, double quantity, String unit) {
-        this.name = name;
-        this.quantity = quantity;
-        this.unit = unit;
-    }
-
-    public String getName() { return name; }
-    public double getQuantity() { return quantity; }
-    public String getUnit() { return unit; }
-
-    public void setQuantity(double quantity) { this.quantity = quantity; }
-    public void setUnit(String unit) { this.unit = unit; }
-
-    @Override
-    public String toString() {
-        return quantity + " " + unit + " of " + name;
-    }
+    
 }
