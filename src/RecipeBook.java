3275bb7bd0c77c0683ae4e7c41555d988a030f33
Zachary Amith <76545253+RogueZach@users.noreply.github.com>
2026-04-25 01:37:32 +0000

    Load five starter recipes in RecipeBook constructor

--- src/RecipeBook.java
+++ src/RecipeBook.java
@@ -12,6 +12,68 @@ public class RecipeBook {
 
     public RecipeBook() {
         recipes = new LinkedList<>();
+        loadStarterRecipes();
+    }
+
+    /**
+     * Hardcodes a small starter set of recipes so the application
+     * has content to demonstrate immediately. In a future version
+     * these would be loaded from a file.
+     */
+    private void loadStarterRecipes() {
+        recipes.add(new Recipe(
+            "Scrambled Eggs",
+            new Ingredient[] {
+                new Ingredient("eggs", 3, ""),
+                new Ingredient("butter", 1, "tbsp"),
+                new Ingredient("salt", 1, "pinch")
+            },
+            10, "quick"
+        ));
+
+        recipes.add(new Recipe(
+            "Pancakes",
+            new Ingredient[] {
+                new Ingredient("flour", 1.5, "cups"),
+                new Ingredient("milk", 1.25, "cups"),
+                new Ingredient("eggs", 1, ""),
+                new Ingredient("sugar", 2, "tbsp"),
+                new Ingredient("baking powder", 1, "tbsp")
+            },
+            20, "breakfast"
+        ));
+
+        recipes.add(new Recipe(
+            "Vegan Pasta",
+            new Ingredient[] {
+                new Ingredient("pasta", 200, "grams"),
+                new Ingredient("olive oil", 2, "tbsp"),
+                new Ingredient("garlic cloves", 2, ""),
+                new Ingredient("tomato sauce", 1, "cup")
+            },
+            25, "vegan"
+        ));
+
+        recipes.add(new Recipe(
+            "Grilled Cheese",
+            new Ingredient[] {
+                new Ingredient("bread slices", 2, ""),
+                new Ingredient("cheddar", 2, "slices"),
+                new Ingredient("butter", 1, "tbsp")
+            },
+            8, "quick"
+        ));
+
+        recipes.add(new Recipe(
+            "Omelette",
+            new Ingredient[] {
+                new Ingredient("eggs", 3, ""),
+                new Ingredient("milk", 2, "tbsp"),
+                new Ingredient("cheddar", 1, "slice"),
+                new Ingredient("butter", 1, "tbsp")
+            },
+            12, "breakfast"
+        ));
     }
 
     /** Adds a recipe to the book. */
